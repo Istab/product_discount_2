f981621f8605668544ee9d8a10108ff1670a3d4b
Istabraq <angel.ofmoons@gmail.com>
2017-06-18 18:35:00 -0400

    Show submitted data unformatted

--- display_discount.php
+++ display_discount.php
@@ -1,3 +1,9 @@
+<?php
+    $product_description = $_POST['product_description'];
+    $list_price = $_POST['list_price'];
+    $discount_percent = $_POST['discount_percent'];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,13 +15,13 @@
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product_description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $list_price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $discount_percent; ?></span><br>
 
         <label>Discount Amount:</label>
         <span><?php echo ''; ?></span><br>
